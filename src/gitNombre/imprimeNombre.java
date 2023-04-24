c6a0d631d7a560f7d28abed1ea8bd58798440365
csrnovelo <cesarnovelo99@gmail.com>
2023-04-24 06:34:25 -0600

    no hay cambios

---
+++ src/gitNombre/imprimeNombre.java
@@ -0,0 +1,11 @@
+package gitNombre;
+
+public class imprimeNombre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("César Novelo");
+	}
+
+}
